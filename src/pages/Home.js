45f413f6149be0360e13eac808b4ec09ef5c4801
Rohan-Samanta <105305746+Rohan-Samanta@users.noreply.github.com>
2023-07-14 16:59:51 +0000

    project setup

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import video from "../components/Lecture 01 Part 3.mp4";
+const Home = () => {
+  return (
+    <>
+      <h1>enjoy your video</h1>
+      <ReactPlayer url='https://www.youtube.com/watch?v=Urwuq45j6q4' />
+      <h1>enjoy your video</h1>
+    </>
+  );
+};
+
+export default Home;
